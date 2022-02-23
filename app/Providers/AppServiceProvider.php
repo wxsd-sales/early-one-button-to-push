a93c2242dc9d8b62641c40a90b27c96b0cfc1b82
Ashesh Singh <ask@mrsinh.com>
2022-02-22 20:26:46 -0600

    Inject additional config from `composer.json` and `mix-minifest.json`

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (file_exists(base_path('composer.json'))) {
+            $composer = json_decode(file_get_contents(base_path('composer.json')), true);
+
+            if (!empty($composer)) {
+                Log::info("Injecting additional config from " . base_path('composer.json'));
+
+                foreach (['name', 'description', 'version', 'type', 'keywords'] as $field) {
+                    if (empty(config("app.$field")) && array_key_exists($field, $composer)) {
+                        config(["app.$field" => $composer[$field]]);
+                    }
+                }
+            }
+        }
+
+        if (file_exists(public_path('mix-manifest.json'))) {
+            $mix = json_decode(file_get_contents(public_path('mix-manifest.json')), true);
+
+            if (!empty($mix)) {
+                Log::info("Injecting additional config from " . public_path('mix-manifest.json'));
+
+                config(["app.mix-manifest" => $mix]);
+            }
+        }
     }
 }
