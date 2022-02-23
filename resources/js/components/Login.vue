39035b2a9db1b76d60ab645c4e1b368ec779c6a6
Ashesh Singh <ask@mrsinh.com>
2022-02-22 20:06:01 -0600

    Remove redundant column

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,61 +4,57 @@
         <div class="columns is-centered is-mobile my-1 py-6">
             <div class="column is-two-thirds">
                 <!-- Email -->
-                <div class="columns is-mobile mb-4 pb-4">
+                <h2 class="is-size-2">
+                    Your Login Token
+                </h2>
+                <p class="subtitle">
+                    Provide the login token sent to you on Webex.
+                </p>
+                <form
+                    class="columns"
+                    method="POST"
+                    :action="url['login']"
+                >
+                    <input
+                        type="hidden"
+                        name="_token"
+                        :value="csrf"
+                    >
                     <div class="column">
-                        <h2 class="is-size-2">
-                            Your Login Token
-                        </h2>
-                        <p class="subtitle">
-                            Provide the login token sent to you on Webex.
-                        </p>
-                        <form
-                            class="columns"
-                            method="POST"
-                            :action="url['login']"
+                        <b-field
+                            label="Token"
+                            label-position="on-border"
+                            custom-class="is-large"
+                            :type="error['email'] ? 'is-danger': token ? 'is-success' : ''"
+                            :message="error['email'] ? error['email'][0] + ' Kindly retry.' : ''"
                         >
-                            <input
-                                type="hidden"
-                                name="_token"
-                                :value="csrf"
-                            >
-                            <div class="column">
-                                <b-field
-                                    label="Token"
-                                    label-position="on-border"
-                                    custom-class="is-large"
-                                    :type="error['email'] ? 'is-danger': token ? 'is-success' : ''"
-                                    :message="error['email'] ? error['email'][0] + ' Kindly retry.' : ''"
-                                >
-                                    <b-input
-                                        size="is-large"
-                                        icon="key"
-                                        name="token"
-                                        type="password"
-                                        custom-class="is-rounded"
-                                        :value="token"
-                                        required
-                                    />
-                                </b-field>
-                            </div>
-                            <div
-                                v-if="!token || error['token']"
-                                class="column is-4"
-                            >
-                                <b-button
-                                    type="is-link"
-                                    native-type="submit"
-                                    size="is-large"
-                                    icon-right="chevron-right"
-                                    class="is-rounded"
-                                    expanded
-                                >
-                                    Login
-                                </b-button>
-                            </div>
-                        </form>
+                            <b-input
+                                size="is-large"
+                                icon="key"
+                                name="token"
+                                type="password"
+                                custom-class="is-rounded"
+                                :value="token"
+                                required
+                            />
+                        </b-field>
                     </div>
-                </div>
+                    <div
+                        v-if="!token || error['token']"
+                        class="column is-4"
+                    >
+                        <b-button
+                            type="is-link"
+                            native-type="submit"
+                            size="is-large"
+                            icon-right="chevron-right"
+                            class="is-rounded"
+                            expanded
+                        >
+                            Login
+                        </b-button>
+                    </div>
+                </form>
             </div>
         </div>
     </section>
