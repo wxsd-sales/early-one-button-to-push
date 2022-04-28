c9b8f9fc8743bd41f86545f5f25261f1441357a5
Ashesh Singh <ask@mrsinh.com>
2022-04-27 23:59:42 -0500

    Update formatting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -39,7 +39,6 @@ class AppServiceProvider extends ServiceProvider
                         config(["app.$field" => $composer[$field]]);
                     }
                 }
-
             }
         };
 
